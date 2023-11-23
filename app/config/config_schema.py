0233d111a03617a7715e66b5412f69e39ea18665
Vincent Prunet <vincent.prunet@mercantour-parcnational.fr>
2023-11-23 17:48:50 +0100

    SQLALCHEMY_DATABASE_URI validation

--- app/config/config_schema.py
+++ app/config/config_schema.py
@@ -5,8 +5,4 @@ from marshmallow.validate import OneOf, Regexp, Email, Length
 class ConfigSchema(Schema):
     SQLALCHEMY_DATABASE_URI = fields.String(
         required=True,
-        validate=Regexp(
-            "^postgresql:\/\/.*:.*@[^:]+:\w+\/\w+",
-            error="Database uri is invalid ex: postgresql://monuser:monpass@server:port/db_name",
-        ),
     )
