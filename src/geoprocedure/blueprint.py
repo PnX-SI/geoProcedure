7dd77ed51c9ceb7ff8b42f0c32ef469073fad3f2
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2023-11-23 18:20:07 +0100

    merge

--- src/geoprocedure/blueprint.py
+++ src/geoprocedure/blueprint.py
@@ -2,18 +2,18 @@ from flask import Blueprint, request
 from werkzeug.datastructures import MultiDict
 
 
-from app.env import db
-from app.models import Demarche, Dossier, Champs, GeoArea, File
-from app.schemas import DemarcheSchema, DossierSchema
-
 routes = Blueprint("main", __name__)
+routes = Blueprint("main", __name__)
+
+from geoprocedure.env import db
+from geoprocedure.models import Demarche, Dossier, Champs, GeoArea, File
+from geoprocedure.schemas import DemarcheSchema, DossierSchema
 
 
 @routes.route("/demarches", methods=["GET"])
 def get_all_demarches():
     params = MultiDict(request.args)
 
-    fields = params.pop("fields", default=[])
     query = db.session.select(Demarche)
     data = db.session.execute(query).scalars()
     return DemarcheSchema().dump(data, many=True)
@@ -21,6 +21,8 @@ def get_all_demarches():
 
 @routes.route("/dossiers", methods=["GET"])
 def get_all_dossiers():
+    params = MultiDict(request.args)
+
     query = db.session.select(Dossier)
     data = db.session.execute(query).scalars()
     return DossierSchema().dump(data, many=True)
